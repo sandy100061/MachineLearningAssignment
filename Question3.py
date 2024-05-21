1839eb808b6a273e9e804a8cfb5da0194c48ca3a
sandy100061 <sandy100060@gmail.com>
2024-05-21 13:12:29 -0500

    Completed Question3

---
+++ Question3.py
@@ -0,0 +1,27 @@
+
+print('---------------Question 3 -> Point 1-----------------')
+print('Creating typles for brothers and sisters')
+sisters = ('Daisy', 'UdayShri', 'ArunaShri')
+brothers = ('Sachin', 'Suresh')
+print('')
+
+#----------------------------------------------------
+print('---------------Question 3 -> Point 2-----------------')
+print('Joining brothers and sisters tuples')
+siblings = sisters + brothers
+print('')
+
+#----------------------------------------------------
+print('---------------Question 3 -> Point 3-----------------')
+print(f'Total Siblings : {len(siblings)}')
+print(siblings)
+print('')
+
+#----------------------------------------------------
+print('---------------Question 3 -> Point 4-----------------')
+family_members = siblings + ('SRK', 'Vijaya')
+print(f'Family Members Count : {len(family_members)}')
+print(family_members)
+print('')
+#----------------------------------------------------
+
