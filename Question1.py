e87d0a202dcd574d538daee2f8768480c6adc99d
sandy100061 <sandy100060@gmail.com>
2024-05-21 12:36:06 -0500

    Modified Changes for Question1

--- Question1.py
+++ Question1.py
@@ -1,3 +1,4 @@
+print('---------------Question 1 -> Point 1-----------------')
 ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
 ages.sort()
 print(f'Sorted Array : {ages}')
@@ -5,13 +6,18 @@ minAge = ages[0]
 maxAge = ages[-1]
 print(f'Minimum val: {minAge}')
 print(f'Maximum val: {maxAge}')
+print('')
 
 #----------------------------------------------------
+print('---------------Question 1 -> Point 2-----------------')
 ages.append(minAge)
 ages.append(maxAge)
+ages.sort()
 print(f'Array after adding minAge and maxAge {ages}')
+print('')
 
 #----------------------------------------------------
+print('---------------Question 1 -> Point 3-----------------')
 middle:int = 0
 arrayLen = len(ages)
 if arrayLen % 2 == 0:
@@ -22,20 +28,25 @@ else:
 
 # medianAge = ages[middle]
 print(f'MedianAge : {medianAge}')
+print('')
 
 #----------------------------------------------------
+print('---------------Question 1 -> Point 4-----------------')
 sum = 0
 for item in ages:
     sum += item
 average = sum / arrayLen
 print(f'Average is {average}')
+print('')
 
 #----------------------------------------------------
+print('---------------Question 1 -> Point 5-----------------')
 ages.sort()
 min = ages[0]
 max = ages[-1]
 range = max - min
 print(f'Range of ages is {range}')
+print('')
 
 #----------------------------------------------------
 
---
+++ Question2.py
@@ -0,0 +1,9 @@
+
+dog = {}
+
+#-------------------------------
+dog['name'] = 'Daisy'
+dog['color'] = 'Black'
+dog['breed'] = 'Labrador Retriever'
+dog['legs'] = 4
+dog['age'] = 14
